2d0c1a1a5a54997581eaa24d1ea915e112eea188
malik <hassanimtiaz158@gmail.com>
2026-09-12 13:45:54 +0500

    API issue solved

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://fakestoreapi.com/products";
+const API_URL = "https://dummyjson.com/products";
 
 const searchBar = document.getElementById("searchbar");
 const favoriteButton = document.getElementById("favbutton");
